233ca41a34a46bbb22eadad0ebde6a26fe9f5d97
Michael Cotugno <michaelcotugno@Michaels-MacBook-Air.local>
2023-03-06 18:05:56 +0100

    slef works from 6 march

---
+++ 0603selfwork.js
@@ -0,0 +1,147 @@
+// Esercizi di riscaldamento:
+// - Scrivere una funzione che prenda in ingresso una stringa e mi restituisca solo la prima parola
+console.log("Esercizio di riscaldamento 1");
+function firstWord(str) {
+    let word = str.split(" ");
+    return word[0];
+}
+console.log(firstWord("My name is Johnny 5!"));
+
+// - Scrivere una funzione che prenda in ingresso un numero N e mi restituisca un array contenente N numeri casuali (compresi tra 1 e 10) ordinati in ordine crescente
+console.log("Esercizio di riscaldamento 2");
+function randomOrdered(n) {
+    let numbers = [];
+    for (let i = 0; i <= n; i++) {
+        numbers.push(Math.floor(Math.random() * (10 - 1 + 1) + 1));
+    }
+    return numbers.sort((a, b) => a - b);
+}
+console.log(randomOrdered(8));
+
+// - Scrivere una funzione che prenda in ingresso un array di numeri e mi restituisca un nuovo array solo con i numeri maggiori di 10
+console.log("Esercizio di riscaldamento 3");
+function moreThan10(arr) {
+    return arr.filter((el) => {
+        if (el >= 10) {
+            return el;
+        }
+    });
+}
+console.log(moreThan10([1, 2, 34, 5, 67, 8, 90]));
+
+// Esercizi intermedi:
+// - Scrivere una funzione che prenda in ingresso un array di stringhe e mi restituisca un nuovo array con le stesse stringhe ma in Uppercase
+console.log("Esercizio intermedi 1");
+function makeUpperCase(str) {
+    return str.map((el) => el.toUpperCase());
+}
+console.log(
+    makeUpperCase([
+        "Hi my name is",
+        "what?",
+        "my name is",
+        "who?",
+        "my name is",
+        "slim shady",
+    ])
+);
+
+// - Realizzare un oggetto che rappresenti una persona. Questo oggetto dovra' avere almeno 3 proprieta' che rappresentino un nome, un cognome e un'eta'. Realizzare un metodo che permetta alla persona di presentarsi.
+// Es. Ciao! Il mio nome e' Matteo Sisto ed ho 36 anni
+console.log("Esercizio intermedi 2");
+let person = {
+    nome: "Michael Dino",
+    cognome: "Cotugno",
+    eta: 39,
+    // details: [{ nome: "Michael Dino", cognome: "Cotugno", eta: 39 }],
+    intro: function () {
+        console.log(
+            `Ciao! Il mio nome è ${this.nome} ${this.cognome} ed ho ${this.eta} anni`
+        );
+        // return `Ciao! Il mio nome è ${this.nome} ${this.cognome} ed ho ${this.eta} anni`;
+    },
+};
+person.intro();
+// console.log(person.intro());
+
+// - Scrivere una funzione che prenda in ingresso un array di dati e per ogni dato mi stampi in console il suo tipo
+console.log("Esercizio intermedi 3");
+function typeofPrinter(arr) {
+    return arr.forEach((el) => console.log(typeof el));
+}
+typeofPrinter([3, "ciao", null, NaN, "ajh", 87]);
+
+// Esercizi che si vabbe' ciaone proprio:
+// - Realizzare un oggetto che rappresenti un astuccio di pennarelli. Questo oggetto dovra' avere una proprieta' che rappresenti una lista di colori sotto forma di stringa. Aggiungere un metodo che ci permetta di rimuovere un colore ed un metodo che ci permetta di aggiungere un colore ma eviti le duplicazioni
+// Es.1     colors : ['rosso', 'verde', 'blu']
+//  astuccio.addColor('giallo')
+//  colors : ['rosso', 'verde', 'blu', 'giallo]
+// Es.2     colors : ['rosso', 'verde', 'blu']
+//  astuccio.addColor('rosso')
+//  Spiacente colore gia' presente nell'astuccio
+console.log("Esercizi che si vabbe' ciaone proprio 1");
+let astuccio = {
+    colors: ["rosso", "verde", "blu"],
+    addColor: function (color) {
+        return this.colors.includes(color)
+            ? console.log("This color is already in the list")
+            : this.colors.push(color);
+    },
+    removeColor: function (color) {
+        return this.colors.includes(color)
+            ? this.colors.splice(this.colors.indexOf(color), 1)
+            : console.log("This color is not in the list");
+    },
+};
+astuccio.addColor("viola");
+console.log(astuccio.colors);
+astuccio.removeColor("viola");
+console.log(astuccio.colors);
+
+// - Scrivere una funzione che prenda in ingresso un array di oggetti, ciascun oggetto dovra' rappresentare un contatto telefonico con le proprieta' nome e numero. La funzione ci dovra' restituire un nuovo array con soltanto i nomi dei contatti
+// Es.   input = [
+//                          {name : 'Sidious' , number : 33333333} ,
+//                          {name : 'Maul' , number : 33344444}
+//                          }
+
+//           output = ['Sidious', 'Maul']
+console.log("Esercizi che si vabbe' ciaone proprio 2");
+let contatti = [
+    { name: "Sidious", number: 33333333 },
+    { name: "Maul", number: 33344444 },
+];
+
+function onlyNames(arr) {
+    return arr.map((el) => el.name);
+    // .filter() only works on arrays, not objects
+    // return arr.filter((el) => el.name);
+}
+console.log(contatti);
+console.log(onlyNames(contatti));
+
+//           Esercizi Manipolazione del DOM:
+// - Scrivere un paragrafo con la scritta : 'Odio JavaScript' ed un bottone. Al click sul bottone la scritta dovrà essere sostituita con: 'Amo JavaScript!!!'
+console.log("Esercizi Manipolazione del DOM 1");
+let JSchng = document.querySelector(".JSchng");
+let oddio = document.querySelector(".oddio");
+JSchng.addEventListener("click", () => {
+    oddio.innerHTML = "Amo JavaScript!!";
+});
+
+// - Dato un array di nomi, fare in modo di creare una ul contenente un li PER OGNI nome che mi permetta di visualizzarli da browser
+console.log("Esercizi Manipolazione del DOM 2");
+let names = ["Michael", "Nicola", "Laetitia", "Annalisa"];
+let nameslist = document.querySelector("nameslist");
+function viewnames(arr) {
+    create;
+    arr.forEach((el) => {
+        nameslist.innerHTML = "<ul><li>";
+    });
+}
+
+// - Realizzare 3 paragrafi e 3 bottoni in HTML. Intervenendo con JS fare in modo che cliccando sul primo bottone cambino i colori di tutti i paragrafi e tornino come prima, al click sul secondo bottone i paragrafi dovranno essere in grassetto e tornare normali e l'ultimo bottone dovra' far scomparire e riapparire i paragrafi
+console.log("Esercizi Manipolazione del DOM 3");
+
+// Esercizio che si vabbe' Matteo ma tu ci vuoi male (usate quello che sapete dei colori in CSS):
+// - Realizzare un div in HTML, dargli un'altezza di 300px, una width di 300px e un bordo 2px solid black in css. Sotto il div realizzare un button. Intervenendo con JS fare in modo che al click sul bottone il background del div cambi con un colore casuale
+console.log("Esercizio che si vabbe' Matteo ma tu ci vuoi male");
--- codewarsTests.js
+++ codewarsTests.js
@@ -53,12 +53,100 @@ var summation = function (num) {
 };
 console.log(summation(8));
 
-function century(num) {
-    return num.splice(2);
-}
-console.log(century(1888));
+// function century(num) {
+//     return num.splice(2);
+// }
+// console.log(century(1888));
 
-function basicOp(operation, value1, value2) {
-    return value1 + +operation + value2;
+// function basicOp(operation, value1, value2) {
+//     return value1 + +operation + value2;
+// }
+// console.log("+", 2, 3);
+
+// function fakeBin(x) {
+//     let a = x.split("").map((el) => +el);
+//     console.log(a);
+//     console.log(a.forEach((el) => (el < 5 ? 0 : 1)));
+// }
+
+let x = 87753267834;
+let a = x
+    .toString()
+    .split("")
+    .map((el) => +el);
+
+console.log(a);
+
+let b = a.map((el) => {
+    if (el < 5) {
+        return 0;
+    } else {
+        return 1;
+    }
+});
+
+let c = a.map((el) => (el < 5 ? 0 : 1));
+
+console.log(b);
+console.log(c);
+
+// const reverseSeq = (n) => {
+//     let a = [];
+//     for (let i = n; i < 0; i--) {
+//         a.push(i);
+//     }
+//     return a;
+// };
+
+// let empty = [];
+// empty.push(3);
+// console.log(empty);
+function countDown(ge) {
+    let a = [];
+    for (let i = ge; i > 0; i--) {
+        a.push(i);
+    }
+    return a;
 }
-console.log("+", 2, 3);
+console.log(countDown(4));
+
+function bmi(weight, height) {
+    let cal = weight / (height * height);
+    switch (true) {
+        case cal <= 18.5:
+            return "Underweight";
+            break;
+        case cal <= 25:
+            return "Normal";
+            break;
+        case cal <= 30:
+            return "Overweight";
+            break;
+        default:
+            return "Obese";
+            break;
+    }
+}
+let hhh = bmi(87, 1.87);
+console.log(hhh);
+
+var cubeChecker = function (volume, side) {
+    if (volume <= 0) {
+        return false;
+    } else if (volume == Math.pow(side, 3)) {
+        return true;
+    } else {
+    }
+    return false;
+};
+
+function barTriang(p1, p2, p3) {
+    let bari = [];
+    for (let i = 0; i < p1.length; i++) {
+        let center = ((p1[i] + p2[i] + p3[i]) / 3).toFixed(4);
+        bari.push(center);
+    }
+    return bari;
+}
+
+console.log(barTriang([1, 2], [6, 5], [7, 8]));
